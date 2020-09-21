c6c7bcd1b25e46504f48d732ef66aa580f18c7af
zer0 <matteo@dusk.network>
2020-09-21 11:22:05 +0200

    Replace license headers and add `dusk-analyzer`
    The tool ensures the correctness of source files' license headers
    
    - Replace license headers with MPL 2.0 standard format
    - Add `dusk-analyzer` subcommand to github actions
    - Add rust-toolchain file
    - Remove Clippy from CI

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
 // Copyright (c) DUSK NETWORK. All rights reserved.
-// Licensed under the MPL 2.0 license. See LICENSE file in the project root for details.
 
 //! ![Build Status](https://travis-ci.com/dusk-network/dusk-pki.svg?branch=master)](https://travis-ci.com/dusk-network/dusk-pki)
 //! ![Repository](https://dusk-network.github.io/dusk-pki/repo-badge.svg)](https://github.com/dusk-network/dusk-pki)
