668f3d7491cf84c448154409b3d081abdb1dc0c2
Victor Lopez <victor@dusk.network>
2020-11-17 20:29:08 +0100

    No-Std compatibility

--- src/errors.rs
+++ src/errors.rs
@@ -4,34 +4,28 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
-use std::io;
-use thiserror::Error;
+#[cfg(feature = "std")]
+use std::fmt;
 
 /// Errors for Dusk PKI
-#[derive(Error, Debug)]
+#[derive(Copy, Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub enum Error {
-    /// Invalid Compressed Point
-    #[error("Invalid Compressed Point Provided")]
+    /// Invalid Compressed Point"
     InvalidPoint,
-    #[error("Invalid Parameters provided to the function")]
     /// Invalid Parameters
     InvalidParameters,
     /// Bad Length
-    #[error("Bad Length (expected {expected:?}, got {found:?})")]
     BadLength {
         /// The found length
         found: usize,
         /// The expected length
         expected: usize,
     },
-    // TODO: this should be handled better, it's too generic
-    #[doc(hidden)]
-    #[error("Invalid I/O operation")]
-    Io(#[from] io::Error),
 }
 
-impl From<Error> for io::Error {
-    fn from(err: Error) -> io::Error {
-        io::Error::new(io::ErrorKind::Other, format!("{}", err))
+#[cfg(feature = "std")]
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Dusk PKI Error: {:?}", &self)
     }
 }
